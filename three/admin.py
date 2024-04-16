d33edea281c16706f9f69083985b544765d1f666
Pavel2232 <pavelmazikinvladimirovich@gmail.com>
2024-04-16 19:18:01 +0300

    Add Admin Panel Page

--- three/admin.py
+++ three/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
 
+from three.models import Page
+
+
 # Register your models here.
+@admin.register(Page)
+class PageAdmin(admin.ModelAdmin):
+    list_display = ('name', 'get_full_url', 'parent')
+    list_filter = ('parent', 'name')
+    readonly_fields = ('url',)
+
+    def get_full_url(self, obj):
+        return obj.get_full_url()
+    get_full_url.short_description = 'Полный URL-адрес'
